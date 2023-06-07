36e2343370b65295cdf7c311056714282d036068
iameugn <metajjuxhin@gmail.com>
2023-06-07 17:52:59 +0200

    Changes

--- content.js
+++ content.js
@@ -1,51 +1,77 @@
 // Enable or disable the functionality based on a condition
 var isEnabled = true; // Set this variable to true or false based on your condition
 
-// Mutation observer to check for changes in the DOM and remove target="_blank" links
-var observer = new MutationObserver(removeTargetBlankLinks);
-
-// Check if the functionality is enabled
-if (isEnabled) {
-  // Override the window.open function to block popups
-  window.open = function () {
-    console.log("Popup blocked");
-  };
-
-  // Override the window.openDatabase function to block database creation
-  window.openDatabase = null;
-
-  // Override the window.openDialog function to block dialog creation
-  window.openDialog = null;
-
-  // Override the window.showModalDialog function to block modal dialog creation
-  window.showModalDialog = null;
-
-  // Override the Element.prototype.open function to block opening new tabs
-  Element.prototype.open = function () {
-    console.log("New tab blocked");
-  };
-
-  // Disable the context menu to prevent opening links in new tabs/windows
-  window.addEventListener("contextmenu", function (event) {
-    event.preventDefault();
-  });
-
-  // Observe the document body for changes and remove target="_blank" links
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-    attributeFilter: ["href"],
-  });
-
-  // Remove target="_blank" links on initial page load
-  removeTargetBlankLinks();
-}
-
-// Function to remove target="_blank" links
+// Remove target="_blank" links
 function removeTargetBlankLinks() {
   var links = document.querySelectorAll('a[target="_blank"]');
-  links.forEach(function (link) {
+  links.forEach(function(link) {
     link.removeAttribute("target");
   });
 }
+
+// Mutation observer to check for changes in the DOM and remove target="_blank" links
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    removeTargetBlankLinks();
+  });
+});
+
+// Function to block new tab creation
+function blockNewTab(event) {
+  event.preventDefault();
+  console.log("New tab blocked");
+}
+
+// Toggle the extension's enabled state
+function toggleEnabled() {
+  isEnabled = !isEnabled;
+
+  if (isEnabled) {
+    // Remove target="_blank" links on initial page load
+    removeTargetBlankLinks();
+
+    // Observe the document body for changes and remove target="_blank" links
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+      attributes: true,
+      attributeFilter: ["href"]
+    });
+
+    // Override the window.open function to block popups
+    window.open = blockNewTab;
+    // Override the window.openDatabase function to block database creation
+    window.openDatabase = null;
+    // Override the window.openDialog function to block dialog creation
+    window.openDialog = null;
+    // Override the window.showModalDialog function to block modal dialog creation
+    window.showModalDialog = null;
+    // Override the Element.prototype.open function to block opening new tabs
+    Element.prototype.open = blockNewTab;
+
+    console.log("Extension enabled");
+  } else {
+    observer.disconnect();
+    window.open = null;
+    window.openDatabase = undefined;
+    window.openDialog = undefined;
+    window.showModalDialog = undefined;
+    Element.prototype.open = undefined;
+
+    console.log("Extension disabled");
+  }
+}
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === "toggleEnabled") {
+    toggleEnabled();
+  } else if (request.type === "removeTargetBlankLinks") {
+    removeTargetBlankLinks();
+  }
+});
+
+// Initialize the extension's state when the document has finished loading
+window.addEventListener("load", function() {
+  toggleEnabled();
+});
--- service_worker.js
+++ service_worker.js
@@ -1,58 +1,13 @@
 // Listen for the browser action being clicked
-chrome.action.onClicked.addListener(function (tab) {
-  if (!tab || !tab.url) {
-    console.error("Invalid tab or tab URL");
-    return;
-  }
-
-  // Get the current tab URL
-  var currentTabUrl = tab.url;
-
-  // Retrieve the list of enabled tabs from storage
-  chrome.storage.local.get("enabledTabs", function (data) {
-    var enabledTabs = data.enabledTabs || [];
-
-    // Check if the current tab is already enabled
-    var isEnabled = enabledTabs.includes(currentTabUrl);
-
-    if (isEnabled) {
-      // Remove the current tab from the enabled tabs list
-      enabledTabs = enabledTabs.filter(function (url) {
-        return url !== currentTabUrl;
-      });
-
-      console.log("Extension disabled for current tab: " + currentTabUrl);
-    } else {
-      // Add the current tab to the enabled tabs list
-      enabledTabs.push(currentTabUrl);
-
-      console.log("Extension enabled for current tab: " + currentTabUrl);
-    }
-
-    // Store the updated enabled tabs list
-    chrome.storage.local.set({ enabledTabs: enabledTabs }, function () {
-      if (chrome.runtime.lastError) {
-        console.error("Failed to store enabled tabs: " + chrome.runtime.lastError.message);
-      }
-    });
-  });
+chrome.action.onClicked.addListener(function(tab) {
+  // Send a message to the content script to toggle the extension's enabled state
+  chrome.tabs.sendMessage(tab.id, { type: "toggleEnabled" });
 });
 
 // Listen for tab updates
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
-    // Execute script in the current tab to remove target="_blank" links
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: removeTargetBlankLinks,
-    });
+    // Send a message to the content script to remove target="_blank" links
+    chrome.tabs.sendMessage(tabId, { type: "removeTargetBlankLinks" });
   }
 });
-
-// Function to remove target="_blank" links
-function removeTargetBlankLinks() {
-  var links = document.querySelectorAll('a[target="_blank"]');
-  links.forEach(function (link) {
-    link.removeAttribute("target");
-  });
-}
